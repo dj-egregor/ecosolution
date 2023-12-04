4715caa935d3e79c1a03335ae767e6981e24d63b
dj-egregor <dj.egregor1@gmail.com>
2023-12-04 21:54:05 +0100

    update

---
+++ src/components/Electricity/Electricity.jsx
@@ -0,0 +1,30 @@
+import CountUp from 'react-countup';
+import Counter from '../Counter/Counter';
+
+import {
+  Container,
+  SubTitle,
+  Wrapper,
+  CounterKwh,
+  Text,
+} from './Electricity.styled';
+
+const Electricity = () => {
+  return (
+    <Container>
+      <SubTitle>
+        Electricity we produced
+        <br />
+        for all time
+      </SubTitle>
+      <Wrapper>
+        <CounterKwh>
+          <Counter></Counter>
+        </CounterKwh>
+        <Text>kWh</Text>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Electricity;
